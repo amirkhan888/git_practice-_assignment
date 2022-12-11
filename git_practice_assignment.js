d991d1be8e437bd8c69194dccd758645e2ce780a
amirkhan888 <mohammadamirkhan7878@gmail.com>
2022-12-11 16:33:53 +0530

    save

---
+++ git_practice_assignment.js
@@ -0,0 +1,14 @@
+let prime = 13
+    let i = 1;
+    let count = 0;
+    while(i<prime){
+        if(prime%i===0){
+            count++;
+        }
+        i++;
+    }
+    if(count===2){
+        console.log("Yes");
+    }else{
+        console.log("No")
+    }
